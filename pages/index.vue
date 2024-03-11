47363cba71dc07260cf8e090940d80a60ee539f8
Aleks <aleksandar.trpkovski@gmail.com>
2024-03-11 18:24:50 +1100

    adjust the layout

--- pages/index.vue
+++ pages/index.vue
@@ -103,7 +103,7 @@ async function resetQuiz() {
                         @click="selectAnswer(index)"
                         :key="`${answer}-${index}`"
                         type="button"
-                        class="inline-flex items-center gap-x-2 rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                        class="inline-flex items-start gap-x-2 rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                         :class="
                             answerPicked
                                 ? answer === correctAnswer
@@ -122,7 +122,7 @@ async function resetQuiz() {
                             class="-ml-0.5 h-5 w-5"
                             aria-hidden="true"
                         />
-                        <span>{{ index + 1 }}.</span> {{ answer }}
+                        <span>{{ index + 1 }}.</span> <span>{{ answer }}</span>
                     </button>
                 </div>
                 <p
